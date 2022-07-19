997f8a7f55236046981a4c90b0441856b78c9c31
LEE-haeuk <rkwksmswhdk@naver.com>
2022-07-19 14:35:05 +0900

    no gitignore

---
+++ src/main/java/com/exerciseday/dev/config/secret/Secret.java
@@ -0,0 +1,5 @@
+package com.exerciseday.dev.config.secret;
+
+public class Secret {
+    public static String JWT_SECRET_KEY = "a5e4a878d43a652c05eef6a63ed70d75e3c29d42f0b0beb103f16623ef9d2a521cc5327e683b39b23a9f36431ad8615cc041bbcd9000e64ca8ae8aab4b9616ba";
+}
