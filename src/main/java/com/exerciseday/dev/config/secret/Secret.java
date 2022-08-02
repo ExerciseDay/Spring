3fce6a4f2dc3f20fc0c162c01c59b78d1d16fc0a
LEE-haeuk <rkwksmswhdk@naver.com>
2022-08-02 16:11:44 +0900

    sub

--- src/main/java/com/exerciseday/dev/config/secret/Secret.java
+++ src/main/java/com/exerciseday/dev/config/secret/Secret.java
@@ -2,4 +2,5 @@ package com.exerciseday.dev.config.secret;
 
 public class Secret {
     public static String JWT_SECRET_KEY = "a5e4a878d43a652c05eef6a63ed70d75e3c29d42f0b0beb103f16623ef9d2a521cc5327e683b39b23a9f36431ad8615cc041bbcd9000e64ca8ae8aab4b9616ba";
+    public static String SNS_TOPIC_ARN = "arn:aws:sns:ap-northeast-1:595431141412:exercisedaytopic";
 }
--- src/main/java/com/exerciseday/dev/utils/SNSService.java
+++ src/main/java/com/exerciseday/dev/utils/SNSService.java
@@ -4,8 +4,16 @@ import java.util.Random;
 
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.exerciseday.dev.config.secret.Secret;
+
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.services.sns.model.SubscribeRequest;
+import software.amazon.awssdk.services.sns.model.SubscribeResponse;
+import software.amazon.awssdk.services.sns.model.ListSubscriptionsRequest;
+import software.amazon.awssdk.services.sns.model.ListSubscriptionsResponse;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sns.SnsClient;
@@ -13,27 +21,49 @@ import software.amazon.awssdk.services.sns.model.PublishRequest;
 import software.amazon.awssdk.services.sns.model.PublishResponse;
 import software.amazon.awssdk.services.sns.model.SnsException;
 
+@Slf4j
 @Service
 public class SNSService {
-
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public String sendSMS(String phone){
+        //전화번호 -> 국제번호 변경
         String phoneNumber = "+82" + phone;
+
+        //인증번호 생성
         String code = "";
         Random random = new Random();
         random.setSeed(System.currentTimeMillis());
         for(int i = 0 ; i < 6 ; i++){
             code+=Integer.toString(random.nextInt(9));
         }
-
-
         String message = "하루운동 본인인증 [" + code + "] 입니다.";
 
+
+        //SNS 접속
         SnsClient snsClient = SnsClient.builder()
         .region(Region.AP_NORTHEAST_1)
         .credentialsProvider(ProfileCredentialsProvider.create())
         .build();
 
+        
+      
+        // 토픽 구독
+        try{
+            SubscribeRequest subscribeRequest = SubscribeRequest.builder()
+            .protocol("sms")
+            .endpoint(phoneNumber)
+            .returnSubscriptionArn(true)
+            .topicArn(Secret.SNS_TOPIC_ARN)
+            .build();
+
+            SubscribeResponse subscribeResponse = snsClient.subscribe(subscribeRequest);
+            System.out.println("Subscription ARN: " + subscribeResponse.subscriptionArn() + "\n\n Status is " + subscribeResponse.sdkHttpResponse().statusCode());
+        }catch (SnsException e){
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }
+
+        // 메세지 전송
         try{
             PublishRequest request = PublishRequest.builder()
             .message(message)
@@ -44,6 +74,18 @@ public class SNSService {
             System.out.println(result.messageId()+" Message sent. Status was " + result.sdkHttpResponse().statusCode());
         } catch (SnsException e){
             System.err.println(e.awsErrorDetails().errorMessage());
+        }
+
+
+        // 토픽 구독 목록
+        try{
+            ListSubscriptionsRequest listSubscriptionsRequest = ListSubscriptionsRequest.builder().build();
+            ListSubscriptionsResponse listSubscriptionsResponse = snsClient.listSubscriptions(listSubscriptionsRequest);
+            System.out.println(listSubscriptionsResponse.subscriptions());
+        } catch (SnsException e) {
+
+            System.err.println(e.awsErrorDetails().errorMessage());
+            
         }
 
         snsClient.close();
