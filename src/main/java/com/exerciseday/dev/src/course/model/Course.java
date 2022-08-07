16318f8f951d784c86a9d91854f9a246366ca030
LEE-haeuk <rkwksmswhdk@naver.com>
2022-08-07 19:00:45 +0900

    코스

---
+++ src/main/java/com/exerciseday/dev/src/course/model/Course.java
@@ -0,0 +1,12 @@
+package com.exerciseday.dev.src.course.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Course {
+    
+}
